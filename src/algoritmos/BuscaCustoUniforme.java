b236a4a157c8bc2d73c8c1a38f09a710a85fbc6f
akiragondo1 <gustavogondo@alunos.utfpr.edu.br>
2018-03-31 19:27:31 -0300

    Insercao da classe BuscaCustoUniforme

--- src/algoritmos/BuscaCustoUniforme.java
+++ src/algoritmos/BuscaCustoUniforme.java
@@ -3,13 +3,13 @@ package algoritmos;
 import arvore.TreeNode;
 import problema.Estado;
 import sistema.Agente;
-import sun.reflect.generics.tree.Tree;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import static comuns.PontosCardeais.*;
 import static java.lang.Math.abs;
+import static java.lang.Math.exp;
 
 /**
  * Classe utilizada para construir um plano para o agente baseado em sua crença utilizando a técnica de
@@ -35,47 +35,81 @@ public class BuscaCustoUniforme implements Busca
         this.agente = agente;
         this.root = new TreeNode();
         this.root.setState(this.agente.sensorPosicao());
-        this.adicionarProximoNivel(this.root);
     }
 
 /* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
 
-    public int[] getPlano()
-    {
-        int[] plan = {N,N,N,NE,L,L,L,L,NE,NE,L};
-        return plan;
-    }
+    public int[] getPlano() {
 
-/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
-
-    protected void adicionarProximoNivel(TreeNode currentNode)
-    {
         TreeNode child;
+        List<Integer> plan = new ArrayList<>();
+        List<TreeNode> fronteira = new ArrayList<>();
+        TreeNode currentNodeCheck;
+        boolean foundSolution = false;
+        int minimumCost = 0, counter = 0, objectivePathIndex = 0;
+        fronteira.add(this.root);
+        jaVisitados.add(this.root.getState());
+        while(!foundSolution){
+            for(int i = 0; i < fronteira.size(); i++){
+                if(fronteira.get(minimumCost).getGn() > fronteira.get(i).getGn())
+                    minimumCost = i;
+            }
+            if(this.agente.getProblem().testeObjetivo(fronteira.get(minimumCost).getState())) {
+                foundSolution = true;
+                objectivePathIndex = minimumCost;
+            }
+            else {
+                fronteira.addAll(expandNodeFromPActions(fronteira.get(minimumCost)));
+                jaVisitados.add(fronteira.get(minimumCost).getState());
+                fronteira.remove(minimumCost);
+                minimumCost = 0;
+                counter++;
+            }
+        }
+        currentNodeCheck = fronteira.get(objectivePathIndex);
 
-        this.jaVisitados.add(currentNode.getState());
-        this.agente.getProblem().acoesPossiveis(currentNode.getState());
+        while(currentNodeCheck != root){
+            plan.add(currentNodeCheck.getAction());
+            currentNodeCheck = currentNodeCheck.getParent();
+        }
 
+        int [] actionPlan = new int[plan.size()];
+        for(int i  = 0; i < plan.size(); i++)
+            actionPlan[i] = plan.get(plan.size()-i-1);
+//
+//        for(int i  = 0; i < plan.size(); i++)
+//            System.out.printf("%2d, ", actionPlan[i]);
+
+        return actionPlan;
+    }
+
+    protected List<TreeNode> expandNodeFromPActions(TreeNode currentNode) {
         int acao = 0;
+        List<TreeNode> childList = new ArrayList<TreeNode>();
+        TreeNode child;
+        Estado nextState;
         for (int each : this.agente.getProblem().acoesPossiveis(currentNode.getState())) {
-
-
             if (each != -1) {
-                Estado nextState = this.agente.getProblem().suc(currentNode.getState(), acao);
-
-                if (!this.checarEstadoValido(nextState)) {
+                nextState = this.agente.getProblem().suc(currentNode.getState(), acao);
+                if (!checarEstadoValido(nextState)) {
                     continue;
                 }
+
 
                 child = currentNode.addChild();
                 child.setState(nextState);
                 child.setAction(acao);
                 child.setGnHn(currentNode.getGn() + this.agente.getProblem().obterCustoAcao(currentNode.getState(), acao, child.getState()),0 );
 
-                this.adicionarProximoNivel(child);
+                childList.add(child);
             }
             acao++;
         }
+        return childList;
     }
+/* @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@ */
+
+
 
     protected boolean checarEstadoValido(Estado estado)
     {
